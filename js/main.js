bd375e6d93a7d617ab5897310ba054a4573f992f
Elsayed-Gamal <eheeba@gmail.com>
2022-11-24 22:59:46 +0200

    change logic to make goals depending on objects

--- js/main.js
+++ js/main.js
@@ -1,30 +1,107 @@
-let shortGoal = document.querySelectorAll('.short-time-goals .goal');
-let longGoal = document.querySelectorAll('.long-time-goals .goal');
+let shortTimeGoalsDiv = document.querySelector('#short-time-goals');
+let longTimeGoalsDiv = document.querySelector('#long-time-goals');
 
-// changing progress value and number of short-time goal dynamically with data attribute
+const shortTimeGoals = [
+  {
+    goalName: 'احتراف CSS',
+    progress: '75',
+  },
+  {
+    goalName: 'القبول في اول عمل على Upwork',
+    progress: '50',
+  },
+  {
+    goalName: 'احتراف Javascript',
+    progress: '20',
+  },
+];
+const longTimeGoals = [
+  // {
+  //   goalName: 'دراسة React',
+  //   progress: '0',
+  // },
+  // {
+  //   goalName: 'Top-Rated on Upwork',
+  //   progress: '0',
+  // },
+];
 
-shortGoal.forEach((element, index) => {
-  let progressvalue = element.children[2].getAttribute('data-value');
+// Create short-time goals section
 
-  element.children[2].style.setProperty(
-    '--progress-value',
-    progressvalue + '%'
-  );
-  element.children[1].innerHTML = progressvalue + '%';
+if (shortTimeGoals.length > 0) {
+  let goalsDiv = document.createElement('div');
+  let button = document.createElement('div');
 
-  element.setAttribute('data-index', index + 1);
-});
+  goalsDiv.classList.add('goals');
+  shortTimeGoalsDiv.append(goalsDiv);
 
-// changing progress value and number of long-time goal dynamically with data attribute
+  shortTimeGoals.forEach((goal, index) => {
+    let goalDiv = document.createElement('div');
+    let goalTitleDiv = document.createElement('div');
+    let percentDive = document.createElement('div');
+    let progressDiv = document.createElement('div');
 
-longGoal.forEach((element, index) => {
-  let progressvalue = element.children[2].getAttribute('data-value');
+    goalDiv.classList.add('goal');
+    goalDiv.setAttribute('data-index', index + 1);
+    goalTitleDiv.classList.add('goal-title');
+    goalTitleDiv.textContent = goal.goalName;
+    percentDive.classList.add('percent');
+    percentDive.textContent = goal.progress + '%';
+    progressDiv.classList.add('progress');
+    progressDiv.setAttribute('data-value', goal.progress);
+    progressDiv.style.setProperty('--progress-value', goal.progress + '%');
 
-  element.children[2].style.setProperty(
-    '--progress-value',
-    progressvalue + '%'
-  );
-  element.children[1].innerHTML = progressvalue + '%';
+    goalsDiv.append(goalDiv);
+    goalDiv.append(goalTitleDiv, percentDive, progressDiv);
+  });
 
-  element.setAttribute('data-index', index + 1);
-});
+  button.classList.add('button');
+  button.textContent = 'عرض المزيد';
+  goalsDiv.append(button);
+} else {
+  let noGoalsDiv = document.createElement('div');
+
+  noGoalsDiv.classList.add('no-goals');
+  noGoalsDiv.textContent = 'لم يتم اضافة اهداف بعد';
+  shortTimeGoalsDiv.append(noGoalsDiv);
+}
+
+// Create long-time goals section
+
+if (longTimeGoals.length > 0) {
+  let goalsDiv = document.createElement('div');
+  let button = document.createElement('div');
+
+  goalsDiv.classList.add('goals');
+  longTimeGoalsDiv.append(goalsDiv);
+
+  longTimeGoals.forEach((goal, index) => {
+    let goalDiv = document.createElement('div');
+    let goalTitleDiv = document.createElement('div');
+    let percentDive = document.createElement('div');
+    let progressDiv = document.createElement('div');
+
+    goalDiv.classList.add('goal');
+    goalDiv.setAttribute('data-index', index + 1);
+    goalTitleDiv.classList.add('goal-title');
+    goalTitleDiv.textContent = goal.goalName;
+    percentDive.classList.add('percent');
+    percentDive.textContent = goal.progress + '%';
+    progressDiv.classList.add('progress');
+    progressDiv.setAttribute('data-value', goal.progress);
+    progressDiv.style.setProperty('--progress-value', goal.progress + '%');
+
+    goalsDiv.append(goalDiv);
+    goalDiv.append(goalTitleDiv, percentDive, progressDiv);
+  });
+
+  button.classList.add('button');
+  button.textContent = 'عرض المزيد';
+  goalsDiv.append(button);
+} else {
+  let noGoalsDiv = document.createElement('div');
+
+  noGoalsDiv.classList.add('no-goals');
+  noGoalsDiv.textContent = 'لم يتم اضافة اهداف بعد';
+  longTimeGoalsDiv.append(noGoalsDiv);
+}
